e92b34f46355d20d935424d17b7ac2a38cba6bb4
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-04-03 15:26:21 +0200

    d

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
 
+
+player.setCurrentTime(120.456).then(function(seconds) {
+  // `seconds` indicates the actual time that the player seeks to
+}).catch(function(error) {
+  switch (error.name) {
+    case 'RangeError':
+        // The time is less than 0 or greater than the video's duration
+        break;
+    default:
+        // Some other error occurred
+        break;
+  }
+});
+console.log(player.setCurrentTime);
