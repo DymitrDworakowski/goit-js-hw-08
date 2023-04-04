65645d01c3f293f6df857ed41795bca9ecdbe9d0
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-04-04 19:04:23 +0000

    Deploying to gh-pages from @ DymitrDworakowski/goit-js-hw-08@d700da4aab9f21fa4569f498cd21a672775c7799 🚀

--- 02-video.c2a6dc5a.js
+++
@@ -1,2 +0,0 @@
-player.on("timeupdate",throttle((function(t){const e=JSON.stringify(t);localStorage.setItem(TIME_KEY,e)}),1e3)),function(){if(null===JSON.parse(localStorage.getItem(TIME_KEY)))return;const t=JSON.parse(localStorage.getItem(TIME_KEY)).seconds;t&&player.setCurrentTime(t).then((function(t){})).catch((function(t){t.name}))}();
-//# sourceMappingURL=02-video.c2a6dc5a.js.map
