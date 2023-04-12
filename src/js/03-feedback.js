b47115d97f55141de96ad9aa1e350aac16e0431c
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-04-12 14:39:16 +0200

    3 завдання перероблено

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,46 @@
-import throttle from 'lodash.throttle';
-const form = document.querySelector(".feedback-form");
+// import throttle from 'lodash.throttle';
+// const form = document.querySelector(".feedback-form");
 
-form.addEventListener("submit", throttle(handleSubmit, 500));
+// form.addEventListener("submit", throttle(handleSubmit, 500));
 
-function handleSubmit(event) {
-    event.preventDefault();
-let LOCALSTORAGE_KEY = {
-    "email": form.elements.email.value,
-    "message": form.elements.message.value
-};
-    console.log(LOCALSTORAGE_KEY);
-localStorage.setItem("feedback-form-state", JSON.stringify(LOCALSTORAGE_KEY));
-form.reset();
-}
-
-
-
-// localStorage.setItem(LOCALSTORAGE_KEY, form.elements.message.value );
-// localStorage.setItem(LOCALSTORAGE_KEY, form.elements.email.textContent);
-
-//   form.reset();
-    
-//   if (email.value === "" || massage.value === "") {
-//     return console.log("Поле не може бути пустим!");
-//   }
-
-//   console.log(`Email: ${email.value}, Massage: ${massage.value}`);
-//   event.currentTarget.reset();
-
-//   elements: { username, password }
-//   } = event.currentTarget;
-
-// const settings = {
-//   theme: "dark",
-//   isAuthenticated: true,
-//   options: [1, 2, 3],
+// function handleSubmit(event) {
+//     event.preventDefault();
+// let LOCALSTORAGE_KEY = {
+//     "email": form.elements.email.value,
+//     "message": form.elements.message.value
 // };
+//     console.log(LOCALSTORAGE_KEY);
+// localStorage.setItem("feedback-form-state", JSON.stringify(LOCALSTORAGE_KEY));
+// form.reset();
+// }
 
-// localStorage.setItem("settings", JSON.stringify(settings));
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector(".feedback-form");
+const feedback = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
+
+form.elements.email.value = feedback.email || "";
+form.elements.message.value = feedback.message || "";
+
+form.addEventListener("input",throttle(event => {
+    const LOCALSTORAGE_KEY = {
+    email: form.elements.email.value,
+    message: form.elements.message.value
+    };
+    localStorage.setItem("feedback-form-state", JSON.stringify(LOCALSTORAGE_KEY));
+}, 500)
+);
+
+form.addEventListener("submit", event => {
+event.preventDefault();
+const LOCALSTORAGE_KEY = {
+    email: "",
+    message: ""
+};
+localStorage.setItem("feedback-form-state", JSON.stringify(LOCALSTORAGE_KEY));
+console.log({
+    email: form.elements.email.value,
+    message: form.elements.message.value
+});
+form.reset();
+});
